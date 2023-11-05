e61bf5f96e653c18851ac5ad7a546eebad29269c
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:58:35 -0300

    aumento salarial

---
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe13.py
@@ -0,0 +1,4 @@
+salario = float(input('QUAL O SEU SALARIO ATUAL?:'))
+porcento = (salario*15) / 100
+novo_salario = salario + porcento
+print('SEU NOVO SALARIO COM 15% DE AUMENTO É DE  R${}'.format(novo_salario))
