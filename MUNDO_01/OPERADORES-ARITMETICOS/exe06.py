0ad612548146f9c2707c4c001da0b1f7ca9bd2c5
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:27:20 -0300

    dobro,triplo,raiz

---
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe06.py
@@ -0,0 +1,5 @@
+numero = int (input('DIGITE UM NUMERO:'))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero**(1/2)
+print('O DOBRO DE  {} É {}\nO TRIPLO É {}\nE A RAIZ É {:.2f}'.format(numero,dobro,triplo,raiz))
