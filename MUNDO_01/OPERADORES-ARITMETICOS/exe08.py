7a65f9d4c631245984829ee2897433bd5f5d0d11
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:33:43 -0300

    centimetros e milimetros

---
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe08.py
@@ -0,0 +1,4 @@
+metros = int (input('DIGITE UMA DISTANCIA EM METROS:'))
+cent = metros * 100
+mili = metros * 1000
+print('{} METROS CORRESPONDEM  A {} CENTIMETROS\nE A {} MILIMETROS'.format(metros,cent,mili))
