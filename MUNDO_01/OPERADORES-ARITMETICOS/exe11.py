10bc0c6f24d24760bc4b06b642b98fe004b5775e
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:51:08 -0300

    pintando parede

---
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe11.py
@@ -0,0 +1,5 @@
+largura = int(input('QUAL A LARGURA DA PAREDE?:'))
+altura = int (input('QUAL A ALTURA DA PAREDE?:'))
+area = altura * largura
+tinta = area / 2
+print('PARA UMA AREA DE {} METROS QUADRADOS,SERAO NECESSARIOS {} LITROS DE TINTA'.format(area,tinta))
