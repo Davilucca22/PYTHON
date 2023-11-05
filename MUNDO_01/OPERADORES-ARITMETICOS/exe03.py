39f8886415adbf188b9e99c0f57efeb5a3837177
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:20:30 -0300

    somando numeros

--- MUNDO_01/OPERADORES-ARITMETICOS/exe03.py
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe03.py
@@ -1,3 +1,3 @@
-n1=int(input('digite um numero:'))
-n2=int(input('digite outro numero:'))
+n1 = int (input('digite um numero:'))
+n2 = int (input('digite outro numero:'))
 print('a soma de {} + {} é igual a {}'.format(n1,n2,n1+n2))
