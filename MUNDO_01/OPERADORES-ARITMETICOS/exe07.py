b5a98aa4ed8e91eb2ebae37d17e409c1c2f7af95
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 12:30:28 -0300

    media de notas

---
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe07.py
@@ -0,0 +1,4 @@
+nota_um = float(input('DIGITE A PRIMEIRA NOTA:'))
+nota_dois = float(input('DIGITE A SEGUNDA NOTA:'))
+media = (nota_um + nota_dois) /2
+print('A MÉDIA DO ALUNO É DE {}'.format(media))
