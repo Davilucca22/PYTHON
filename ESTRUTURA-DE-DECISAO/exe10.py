e19ffaa0996f627976792e89788df085aa1a11c8
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-05 10:21:01 -0300

    Update exe10.py

--- ESTRUTURA-DE-DECISAO/exe10.py
+++ ESTRUTURA-DE-DECISAO/exe10.py
@@ -1,6 +1,6 @@
 '''Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.'''
 while True:
-    periodo=str(input('QUAL PERIODO VOCE ESTUDA?\n[M]MATUTINO\n[V]VESPERINO\n[N]NOTURNO\nR:'))
+    periodo=str(input('QUAL PERIODO VOCE ESTUDA?\n[M]MATUTINO\n[V]VESPERINO\n[N]NOTURNO\nR:').upper())
     if periodo == 'M':
         print('BOM DIA!')
         break
