b0beee3bec29c1bcb71c2c803f826a13726011ef
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-12 12:36:15 -0300

    desconto de produto

--- MUNDO_01/OPERADORES-ARITMETICOS/exe12.py
+++ MUNDO_01/OPERADORES-ARITMETICOS/exe12.py
@@ -1,4 +1,4 @@
 valor = float(input('QUAL O VALOR DO PRODUTO?:'))
 desc = (valor*5)/100
 novo_valor= valor - desc
-print('O NOVO VALOR DO PRODUTO COM 5% DE DESCONTO É DE R${}'.format(novo_valor))
+print('O NOVO VALOR DO PRODUTO COM 5% DE DESCONTO É DE R${:.2f}'.format(novo_valor))
