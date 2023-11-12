a7565619de2246a5931388316180296baf924692
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-11-12 14:16:33 -0300

    calculando  a hipotenusa

---
+++ MUNDO_01/MÓDULOS/exe17.py
@@ -0,0 +1,4 @@
+from math import hypot
+oposto = float(input('medida do cateto oposto:'))
+adjacente = float(input('medida do cateto adjacente:'))
+print('a hipotenusa deste triangulo é de {}'.format(hypot(oposto,adjacente)))
