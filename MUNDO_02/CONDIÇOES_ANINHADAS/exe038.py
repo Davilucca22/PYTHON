2174930a13277a1cb0ded1da2e3861c1fc93f0ed
davilucas <91565261+Davilucca22@users.noreply.github.com>
2023-12-02 17:42:43 -0300

    maior numero

---
+++ MUNDO_02/CONDIÇOES_ANINHADAS/exe038.py
@@ -0,0 +1,8 @@
+num_1 = int(input('digite um numero:'))
+num_2 = int(input('digite outro numero:'))
+if num_1 > num_2:
+    print(f'o maior numero é {num_1}!')
+elif num_2 > num_1:
+    print(f'o maior numero é {num_2}!')
+elif num_1 == num_2:
+    print('os dois numeros sao iguais!')
