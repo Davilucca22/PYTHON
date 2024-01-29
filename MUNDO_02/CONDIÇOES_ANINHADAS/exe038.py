26fda43b869cdd8f4cb6d0094cf9c55cb118f678
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-01-29 20:30:13 -0300

    exe038

--- MUNDO_02/CONDIÇOES_ANINHADAS/exe038.py
+++ MUNDO_02/CONDIÇOES_ANINHADAS/exe038.py
@@ -2,7 +2,7 @@ num_1 = int(input('digite um numero:'))
 num_2 = int(input('digite outro numero:'))
 if num_1 > num_2:
     print(f'o maior numero é {num_1}!')
-elif num_2 > num_1:
+elif num_1 < num_2:
     print(f'o maior numero é {num_2}!')
-elif num_1 == num_2:
+else:
     print('os dois numeros sao iguais!')
