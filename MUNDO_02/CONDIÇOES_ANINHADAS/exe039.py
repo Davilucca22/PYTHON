97dad5cf4dc9f46fd666a02a6d42474af6143f5c
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-01-29 20:49:34 -0300

    exe039

--- MUNDO_02/CONDIÇOES_ANINHADAS/exe039.py
+++ MUNDO_02/CONDIÇOES_ANINHADAS/exe039.py
@@ -1,10 +1,11 @@
-from datetime import datetime
-ano_atual = datetime.today().year
+from datetime import date
+
+ano_atual = date.today().year
 nascimento = int(input('em que voce nasceu?:'))
 idade = ano_atual - nascimento
 if idade < 18:
-    print(f'voce devera se alistar em {18-idade} anos!')
+    print(f'voce devera se alistar em {18-idade+ano_atual}!')
 elif idade == 18:
-    print('voce deve se alistar este ano!')
+    print(f'voce deve se alistar este ano {ano_atual}!')
 elif idade > 18:
-    print(f'voce deveria ter se alistado há {idade-18} anos!')
+    print(f'voce deveria ter se alistado em {ano_atual-(idade-18)}!')
