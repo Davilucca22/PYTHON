980274d3514af9ce17d5a37df718543c44ac58da
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-01-29 20:27:00 -0300

    exe036

--- MUNDO_02/CONDIÇOES_ANINHADAS/exe036.py
+++ MUNDO_02/CONDIÇOES_ANINHADAS/exe036.py
@@ -1,9 +1,15 @@
 valor_casa = float(input('qual o valor da casa?:').strip())
+
 salario = float(input('qual o valor do seu salario?:').strip())
+
 anos = int(input('em quantos anos vai pagar as parcelas da casa?:').strip())
+
 prestacao_mensal = (valor_casa / anos) / 12
+
 limite = (salario*30) / 100
+
 if prestacao_mensal >= limite:
     print(f'o valor das parcelas mensais seriam de R${prestacao_mensal:.2f},esse valor excede 30% do seu salario.\nentao seu imprestimo foi \033[031mnegado!'+'\033[m')
+    
 elif prestacao_mensal <= limite:
     print(f'empretimo \033[0;32maceito\033[m!\nvoce deverá pagar R${prestacao_mensal:.2f} por mes,durante {anos} anos')
