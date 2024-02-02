89bf11af1c1182223ee2599f3234502497357ca6
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-01 21:19:17 -0300

    exe55

---
+++ MUNDO_02/ESTRUTURA FOR/exe55.py
@@ -0,0 +1,21 @@
+vet = [0 for x in range (5)]
+
+for c in range(0,5):
+    vet[c] = float(input(f'digite o peso da {c+1}º pessoa:'))
+
+maior = vet[0]
+
+for c in range(0,5):
+    if vet[c] > maior:
+        maior = vet[c]
+
+menor = vet[0]
+
+for c in range(0,5):
+    if vet[c] < menor:
+        menor = vet[c]
+
+print('-'*30)
+print(f'maior peso:{maior}')
+print(f'menor peso:{menor}')
+print('-'*30)
