ef297cc72c88a07f7fba7fb19c1bb8e631bd01ec
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-02 21:02:30 -0300

    impares multiplos de 3

--- MUNDO_02/ESTRUTURA FOR/exe48.py
+++ MUNDO_02/ESTRUTURA FOR/exe48.py
@@ -1,9 +1,8 @@
 soma = 0
 print('soma dos impares multiplos de tres entre 1 e 500:')
 
-for c in range(1,500):
-    if c % 2 != 0:
-        if c % 3 == 0:
-            soma = soma + c
+for c in range(1,501,2):
+    if c % 3 == 0:
+        soma = soma + c
 
 print(soma)
