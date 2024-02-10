cd969acc254140eba263bf52a464697d64f78a8a
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-09 21:41:09 -0300

    exe57

---
+++ MUNDO_02/ESTRUTURA WHILE/exe57.py
@@ -0,0 +1,19 @@
+sexo = str(input('qual o seu sexo?[M/F]:').upper())
+
+if sexo == 'M':
+    print('voce tem pipi')
+elif sexo == 'F':
+    print('voce nao tem pipi')
+else:
+       print('valor invalido')
+
+while sexo != 'M' and sexo != 'F':
+
+    sexo = str(input('qual o seu sexo?[M/F]:').upper())
+
+    if sexo == 'M':
+     print('voce tem pipi')
+    elif sexo == 'F':
+     print('voce nao tem pipi')
+    else:
+       print('valor invalido')
