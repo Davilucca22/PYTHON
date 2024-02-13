4da6e9c203fd7ca8780e54c92c5c6886e9c28738
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-12 21:01:14 -0300

    exe58

--- MUNDO_02/ESTRUTURA WHILE/exe58.py
+++ MUNDO_02/ESTRUTURA WHILE/exe58.py
@@ -2,27 +2,19 @@ from random import randint
 
 alet = randint(0,10)
 
-cont = 0
+cont = 1
 
 print('-'*30)
 tent = int(input('pensei em um numero entre 0 e 10,tente adivinhar:'))
- 
-if tent != alet:
-    print('INCORRETO!')
-    cont += 1
-else:
-    print('CORRETO!!')
 
 while tent != alet:
-    print('-'*30)
-    tent = int(input('tente novamente:'))
-
     if tent != alet:
         print('INCORRETO!')
         cont += 1
-    else:
-        print('CORRETO!!')
+
+    tent = int(input('pensei em um numero entre 0 e 10,tente adivinhar:'))
 
 print('-'*30)
+print('correto!')
 print(f'voce precisou de {cont} palpites para ganhar o jogo')
 print('-'*30)
