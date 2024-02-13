f5732be017b1f48354c1f3a03feee8a860f249dd
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-13 16:24:34 -0300

    sequencia de fibonacci

--- MUNDO_02/ESTRUTURA WHILE/exe63.py
+++ MUNDO_02/ESTRUTURA WHILE/exe63.py
@@ -1,10 +1,17 @@
 numb = int(input('digite um numero:'))
 
-cont = 0
-soma = 1
+t1 = 0
+t2 = 1
+
+print(f'{t1} -> {t2}',end='')
+
+cont = 3
 
 while cont <= numb:
-    fibo = soma + cont
-    print(fibo)
-    cont += 1
-    fibo += cont
+    t3 = t1 + t2
+    print(f'-> {t3} ',end='')
+    cont +=1
+    t1 = t2
+    t2 = t3
+
+print('fim')
