e52c69822ba2a1570d0b0c26354735b26ef3075d
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-13 14:17:04 -0300

    exe64

---
+++ MUNDO_02/ESTRUTURA WHILE/exe64.py
@@ -0,0 +1,14 @@
+numb = int(input('digite um numero:'))
+
+soma = 0
+cont = 0
+
+while numb != 999:
+    cont += 1
+    soma += numb
+    numb = int (input('digite um numero:'))
+
+print('-'*30)
+print(f'foram digitados {cont} numeros,e')
+print(f'a soma dos numeros lidos é {soma}')
+print('-'*30)
