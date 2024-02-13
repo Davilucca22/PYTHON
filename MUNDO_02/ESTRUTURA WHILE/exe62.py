3aa8f4bafa7c39dc4b05d3d25d25c3a593aa16f0
davilucas <91565261+Davilucca22@users.noreply.github.com>
2024-02-13 13:10:29 -0300

    exe62

--- MUNDO_02/ESTRUTURA WHILE/exe62.py
+++ MUNDO_02/ESTRUTURA WHILE/exe62.py
@@ -14,16 +14,16 @@ while cont != 11:
     cont += 1
 
 print('fim')
-print()
-numb = str(input('deseja informar novos numeros[S/N]:').upper())
+print('-'*30)
+print('(digite 0 para parar)')
+ptermo = int (input('digite o primeiro termo:'))
+razao = int (input('digite a razao:'))
 
-while numb == 'S':
-  ptermo = int (input('digite o primeiro termo:'))
-  razao = int (input('digite a razao:'))  
+while ptermo != 0:
 
   soma = 0
   cont = 1
-  
+
   print('-'*30)
   print('primeiros 10 termos da PA')
   print('-'*30)
@@ -31,9 +31,11 @@ while numb == 'S':
    soma = ptermo + (cont - 1) * razao
    print(soma,end=' -> ')
    cont += 1
-   
+
   print('fim')
-  print()
-  numb = str(input('deseja informar novos numeros[S/N]:').upper())
+  print('-'*30)
+  print('(digite 0 para parar)')
+  ptermo = int (input('digite o primeiro termo:'))
+  razao = int (input('digite a razao:'))  
 
 print('fim')
